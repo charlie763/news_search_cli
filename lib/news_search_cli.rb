ea56d615b084806ad72642135efdfe3868608588
charlie763 <cwisoff@gmail.com>
2020-06-15 11:38:10 -0600

    fix initial file structure

---
+++ lib/news_search_cli.rb
@@ -0,0 +1,6 @@
+require "news_search_cli/version"
+
+module NewsSearchCli
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/news_search_cli/version.rb
@@ -0,0 +1,3 @@
+module NewsSearchCli
+  VERSION = "0.1.0"
+end
