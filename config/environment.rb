5d8a21737cdc74271ba5bb49eb295d01ac6e1c21
charlie763 <cwisoff@gmail.com>
2020-06-15 12:16:26 -0600

    add class files and environment config file

---
+++ config/environment.rb
@@ -0,0 +1,8 @@
+require 'open-uri'
+require 'json'
+require 'pry'
+
+require_relative '..lib/article.rb'
+require_relative '..lib/snippet.rb'
+require_relative '..lib/scraper.rb'
+require_relative '..lib/cli.rb'
--- lib/news_search_cli.rb
+++
@@ -1,6 +0,0 @@
-require "news_search_cli/version"
-
-module NewsSearchCli
-  class Error < StandardError; end
-  # Your code goes here...
-end
