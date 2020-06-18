fe84fcfa12d148bc1cefb3f40f5fe56916df8d50
charlie763 <cwisoff@gmail.com>
2020-06-18 11:38:35 -0600

    change file structure to reflect more common gem pattern

---
+++ lib/news_search_cli.rb
@@ -0,0 +1,12 @@
+require 'open-uri'
+require 'json'
+require 'nokogiri'
+require 'date'
+require 'pry'
+
+require_relative './concerns/news_item.rb'
+require_relative './news_search_cli/api_response.rb'
+require_relative './news_search_cli/scraper.rb'
+require_relative './news_search_cli/article.rb'
+require_relative './news_search_cli/snippet.rb'
+require_relative './news_search_cli/cli.rb'
